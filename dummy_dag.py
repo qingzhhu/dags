9963cceeff4320d1c4eb824236732c5f40cc8c06
qingzhhu <45933387+qingzhhu@users.noreply.github.com>
2020-09-26 19:57:10 +0800

    Update dummy_dag.py

--- dummy_dag.py
+++ dummy_dag.py
@@ -32,7 +32,7 @@ default_args = {
     # 'trigger_rule': 'all_success'
 }
 dag = DAG(
-    'tutorial',
+    'dummy-dag',
     default_args=default_args,
     description='A simple tutorial DAG',
     schedule_interval=timedelta(days=1),
